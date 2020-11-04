8b90429d25da93b81734447a04f85b256fd49419
Phillip A. Wessels <phillip.a.wessels@gmail.com>
2020-11-03 18:47:43 -0800

    Blog, variables, etc

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,24 +1,45 @@
-let vidIndex = 1;
-showVid(vidIndex);
-
-function nextVid(vid) {
-  showVid(vidIndex += vid);
-}
-
-function showVid(vid) {
-  let vids = document.getElementsByClassName("gameplay__video")
-  if (vid > vids.length) { vidIndex = 1 }
-  if (vid < 1) { vidIndex = vids.length }
-  Array.prototype.forEach.call(vids, (vid) => {
-    vid.style.display = "none";
-  })
-  vids[vidIndex-1].style.display = "block";
-}
-
 function closeMenu() {
   let toggleInput = document.getElementById('navbar-collapse');
   toggleInput.checked = false;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  let url = 'feed.xml';
+  fetch(url)
+  .then(response => response.text())
+  .then(data => {
+    let parser = new DOMParser();
+    let xml = parser.parseFromString(data, "application/xml");
+    // blog.textContent = data;
+    // console.log(xml);
+    buildBlogPosts(xml);
+  })
+});
+    
+let blog = document.getElementsByClassName('news__blog')[0];
+function buildBlogPosts(xml) {
+  let blogPosts = xml.getElementsByTagName('item');
+  Array.prototype.forEach.call(blogPosts, item => {
+    let blogPost = document.createElement('article');
+    blogPost.setAttribute('class' ,'news__blog-post');
+
+    let title = item.getElementsByTagName('title')[0].textContent;
+
+    let link = item.getElementsByTagName('link')[0].textContent;
+    let creator = item.getElementsByTagName('dc:creator')[0].textContent;
+    let date = item.getElementsByTagName('pubDate')[0].textContent;
+    let category = item.getElementsByTagName('category')[0].textContent;
+    let description = item.getElementsByTagName('description')[0].textContent;
+    let content = item.getElementsByTagName('content:encoded')[0].textContent;
+    blogPost.innerHTML = `
+      <a class="news__blog-post-title" href="${link}"><h3>${title}</h3></a>
+      <div class="news__blog-post-content"><p class="news__blog-post-tags"><a href="${link}" class="news__blog-post-date"><i class="far fa-calendar-alt"></i> ${date}</a><a href="http://tombraider-dox.com/author/${creator}/" class="news__blog-post-author"><i class="fas fa-user"></i> ${creator}</a><a href="http://tombraider-dox.com/category/${category}/" class="news__blog-post-category"><i class="fas fa-tag"></i> ${category}</a></p>
+      ${content}</div>
+    `
+    blog.append(blogPost);
+  });  
+}
+
 
 let gallery = document.getElementsByClassName('images__grid')[0];
 
@@ -88,3 +109,20 @@ function addGalleryImages(aspectRatio) {
 addGalleryImages('landscape');
 addGalleryImages('portrait');
 addGalleryImages('square');
+
+let vidIndex = 1;
+showVid(vidIndex);
+
+function nextVid(vid) {
+  showVid(vidIndex += vid);
+}
+
+function showVid(vid) {
+  let vids = document.getElementsByClassName("gameplay__video")
+  if (vid > vids.length) { vidIndex = 1 }
+  if (vid < 1) { vidIndex = vids.length }
+  Array.prototype.forEach.call(vids, (vid) => {
+    vid.style.display = "none";
+  })
+  vids[vidIndex-1].style.display = "block";
+}
